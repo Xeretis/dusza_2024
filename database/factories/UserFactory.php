5e4478ebdc51126e73aac4efe715a7797f259f76
Máté Gyimesi <gyim.mate@gmail.com>
2024-11-08 15:55:02 +0100

    **REDACTED**

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,17 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            "name" => fake()->name(),
+            "email" => fake()
+                ->unique()
+                ->safeEmail(),
+            "email_verified_at" => now(),
+            "username" => fake()
+                ->unique()
+                ->userName(),
+            "role" => "organizer",
+            "password" => (static::$password ??= Hash::make("password")),
+            "remember_token" => Str::random(10),
         ];
     }
 
@@ -37,8 +43,10 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(
+            fn(array $attributes) => [
+                "email_verified_at" => null,
+            ]
+        );
     }
 }
