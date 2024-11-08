3a1479cb84a8ab17ea0f140b918de723494f323b
Dini <dinicsek01@gmail.com>
2024-11-08 16:29:07 +0100

    **REDACTED**

--- lang/hu/auth.php
+++ lang/hu/auth.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 return [
     'failed'   => 'Rossz email-jelszó páros.',
-    'password' => 'A(z) :attribute jelszónak kell, hogy legyen!',
-    'throttle' => 'Túl sok próbálkozás. Kérjük próbálja újra :seconds másodperc múlva.',
+    'password' => 'A(z) :attribute jelszónak kell lennie!',
+    'throttle' => 'Túl sok próbálkozás. Kérjük próbáld újra :seconds másodperc múlva.',
 ];
