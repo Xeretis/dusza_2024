062e73406992d049ac9e38fa2d1d10c1fa4334a5
Máté Gyimesi <gyim.mate@gmail.com>
2024-11-08 13:58:34 +0100

    **REDACTED**

--- dev.sh
+++ dev.sh
@@ -4,6 +4,6 @@
 # Not the prettiest script, it is definetely hacked together, but I digress. I mean, it does the job
 # Windows users: My sincere apologies, I don't know powershell
 
-tmux new-session php artisan octane:start \; split-window -h "docker compose up" \; split-window -h "sleep 5 && php artisan horizon" \; split-window -d "pnpm dev" \; split-window -v "while :; do php artisan schedule:run; sleep 60; done" \; attach
+tmux new-session php artisan octane:start --watch \; split-window -h "docker compose up" \; split-window -h "sleep 5 && php artisan horizon" \; split-window -d "pnpm dev" \; split-window -v "while :; do php artisan schedule:run; sleep 60; done" \; attach
 
 docker compose down --volumes -t 00
