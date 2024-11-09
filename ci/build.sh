d3cebeb28a318124ce748266f8b5ab45e71faa1a
Máté Gyimesi <gyim.mate@gmail.com>
2024-11-09 17:14:47 +0100

    **REDACTED**

--- ci/build.sh
+++ ci/build.sh
@@ -1,4 +1,4 @@
 mkdir dist
-docker buildx --cache-from type=gha --cache-to type=gha,mode=max bake --load static-builder
-docker cp $(docker create --name static-builder dunglas/frankenphp:static-builder):/go/src/app/dist/frankenphp-linux-$(uname -m) ./dist/larawhale_linux-x86_64
-docker rm static-builder
+docker buildx build --cache-to type=gha --cache-from type=gha,mode=max -t static-laravel-app -f static-build.Dockerfile .
+docker cp $(docker create --name static-laravel-app-tmp static-laravel-app):/go/src/app/dist/frankenphp-linux-x86_64 ./dist/larawhale_linux-x86_64
+docker rm static-laravel-app-tmp
