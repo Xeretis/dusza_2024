44c9ebdd56b78b441c7a8c1f0ab09bd0f9f484a3
Máté Gyimesi <gyim.mate@gmail.com>
2024-11-09 17:07:25 +0100

    **REDACTED**

--- ci/build.sh
+++ ci/build.sh
@@ -1,3 +1,4 @@
-docker buildx bake --load static-builder
+mkdir dist
+docker buildx --cache-from type=gha --cache-to type=gha,mode=max bake --load static-builder
 docker cp $(docker create --name static-builder dunglas/frankenphp:static-builder):/go/src/app/dist/frankenphp-linux-$(uname -m) ./dist/larawhale_linux-x86_64
 docker rm static-builder
