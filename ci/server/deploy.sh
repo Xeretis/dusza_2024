9d0d42a53b4b2e4321364ad2493d8c2591070929
Máté Gyimesi <gyim.mate@gmail.com>
2024-11-09 21:33:32 +0100

    **REDACTED**

--- ci/server/deploy.sh
+++ ci/server/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/dusza/larawhale
 mkdir -p /home/dusza/larawhale/storage
-mkdir -p framework/{sessions,views,cache}
+mkdir -p /home/dusza/larawhale/storage/framework/{sessions,views,cache}
 mv /home/dusza/larawhale/htaccess /home/dusza/www/.htaccess
 mv /home/dusza/larawhale/larawhale.service /home/dusza/.config/systemd/user/larawhale.service
 
