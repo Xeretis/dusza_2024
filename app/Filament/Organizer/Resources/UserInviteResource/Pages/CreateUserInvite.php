0e09a575badff57dfe9ccd652f4c44ecdc0969a1
Máté Gyimesi <gyim.mate@gmail.com>
2024-11-10 02:01:26 +0100

    **REDACTED**

--- app/Filament/Organizer/Resources/UserInviteResource/Pages/CreateUserInvite.php
+++ app/Filament/Organizer/Resources/UserInviteResource/Pages/CreateUserInvite.php
@@ -5,8 +5,18 @@ namespace App\Filament\Organizer\Resources\UserInviteResource\Pages;
 use App\Filament\Organizer\Resources\UserInviteResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class CreateUserInvite extends CreateRecord
 {
     protected static string $resource = UserInviteResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        return static::getModel()::create([
+            ...$data,
+            'token' => Str::random(64),
+        ]);
+    }
 }
