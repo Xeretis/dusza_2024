a005df4dab403983f8db65b6e19b72d48af97d9e
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-11-10 01:41:58 +0100

    **REDACTED**

--- app/Filament/Common/Pages/Auth/Register.php
+++ app/Filament/Common/Pages/Auth/Register.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Common\Pages\Auth;
 
 
+use App\Enums\UserRole;
 use Filament\Forms\Components\Component;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 
@@ -14,6 +16,7 @@ class Register extends \Filament\Pages\Auth\Register
         return $form->schema([
             $this->getUsernameFormComponent(),
             $this->getEmailFormComponent(),
+            $this->getRoleFormComponent(),
             $this->getPasswordFormComponent(),
             $this->getPasswordConfirmationFormComponent(),
         ])->statePath('data');
@@ -27,5 +30,21 @@ class Register extends \Filament\Pages\Auth\Register
             ->maxLength(255)
             ->unique('users', 'username')
             ->autofocus();
+    }
+
+    protected function getRoleFormComponent(): Component
+    {
+        return Select::make('role')
+            ->label('Szerepkör')
+            ->options([
+                UserRole::Competitor->value => UserRole::Competitor->getLabel(),
+                UserRole::Teacher->value => UserRole::Teacher->getLabel()
+            ])
+            ->hintIcon('heroicon-m-information-circle')
+            ->hintIconTooltip('Ha nem versenyzőként vagy tanárként szeretnél regisztrálni, vedd fel a kapcsolatot a szervezőkkel, hogy meghívhassanak!')
+            ->native(false)
+            ->selectablePlaceholder(false)
+            ->required()
+            ->in([UserRole::Competitor->value, UserRole::Teacher->value]);
     }
 }
