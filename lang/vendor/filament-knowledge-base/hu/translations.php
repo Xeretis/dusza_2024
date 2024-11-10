cf38beabd40321b6840022e4ed0d390e64ee7cb3
Máté Gyimesi <gyim.mate@gmail.com>
2024-11-10 16:15:27 +0100

    **REDACTED**

---
+++ lang/vendor/filament-knowledge-base/hu/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'knowledge-base' => 'Dokumentáció',
+    'help' => 'Segítség',
+    'open-documentation' => 'Dokumentáció megnyitása',
+    'close' => 'Bezárás',
+    'back-to-default-panel' => 'Vissza az oldalra',
+    'url-copied' => 'URL másolva a vágólapra.',
+    'code-copied' => 'Kód másolva a vágólapra.',
+];
