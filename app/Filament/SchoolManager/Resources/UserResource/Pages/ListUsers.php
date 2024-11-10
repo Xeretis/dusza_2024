081a4f279acf9e4b913bdbcaf812a1f1f2e413eb
Máté Gyimesi <gyim.mate@gmail.com>
2024-11-10 14:38:21 +0100

    **REDACTED**

--- app/Filament/SchoolManager/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/SchoolManager/Resources/UserResource/Pages/ListUsers.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\SchoolManager\Resources\UserResource\Pages;
 
+use App\Enums\UserRole;
 use App\Filament\SchoolManager\Resources\UserResource;
+use App\Models\UserInvite;
+use App\Notifications\UserInviteNotification;
 use Filament\Actions;
+use Filament\Notifications\Notification as NotificationsNotification;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Notification;
 
 class ListUsers extends ListRecords
 {
@@ -12,6 +17,38 @@ class ListUsers extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [Actions\CreateAction::make()];
+        return [
+            Actions\Action::make('invite')
+                ->label('Felhasználó meghívása')
+                ->modalDescription(
+                    'Kérlek add meg az email címét, amelyre meghívót szeretnél küldeni.'
+                )
+                ->form([
+                    \Filament\Forms\Components\TextInput::make('email')
+                        ->label('Email cím')
+                        ->required()
+                        ->email(),
+                ])
+                ->action(function (array $data) {
+                    $email = $data['email'];
+
+                    $invite = UserInvite::create([
+                        'email' => $email,
+                        'token' => \Illuminate\Support\Str::random(64),
+                        'school_id' => auth()->user()->school_id,
+                        'role' => UserRole::SchoolManager,
+                    ]);
+
+                    Notification::route('mail', $invite->email)->notify(
+                        new UserInviteNotification($invite->token)
+                    );
+
+                    NotificationsNotification::make()
+                        ->title('Felhasználó meghívva')
+                        ->body('A felhasználót sikeresen meghívtad.')
+                        ->success()
+                        ->send();
+                }),
+        ];
     }
 }
